3ed3ce8acca7791be6a5ccc1eef4d6c9bcf1ed0e
YAN GURENKO <yan.gurenko@hotmail.com>
2022-01-23 10:10:52 +0700

    Added main file

---
+++ lexer.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+
+void strcom(char firstarray[], char secondarray[])
+{
+    for (int i = 0; i < 7; i++)
+    {
+        if (firstarray[i] != secondarray[i]) return false;
+    }
+}
+
+
+int main()
+{
+    const int rows = 4;
+    const int columns = 7;
+
+    // matrix of keywords
+    char keywords[rows][columns] = {
+        "int",
+        "float",
+        "double",
+        "char"
+    };
+
+    const char input[] = "test float double char";
+
+    // auxiliary variables | вспомогательные переменные
+    int r = 0;
+    int element = 0;
+    int word = 0;
+
+    /*-----------------------------------------------------------------------------*/
+
+    // rows of matrix | ряды матрицы
+    for (int i = 0; i < sizeof(input); i++)
+    {
+        if (input[i] == ' ') r++;
+    }
+
+
+    r++;
+    char parsed[r][columns];
+
+    // filling matrix | заполнение матрицы
+    for (int i = 0; i < sizeof(input); i++)
+    {
+        if (input[i] == ' ')
+        {
+            if (element < columns)
+            {
+                for (; element < sizeof(keywords); element++) parsed[word][element] = '\0';
+            }
+
+            word++;
+            element = 0;
+        }
+        else
+        {
+            parsed[word][element] = input[i];
+            element++;
+        }
+    }
+
+    /*-----------------------------------------------------------------------------*/
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            if (strcom(parsed[i], keywords[j]) == true) printf("%s\n", parsed[i]);
+        }
+    }
+}
